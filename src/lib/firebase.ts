e28bc9ec2807150bb48c32d71da77f217d9c25d2
Jio Baba <jiobaba369@gmail.com>
2025-08-05 08:06:31 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -8,13 +8,13 @@ import { getFirestore, connectFirestoreEmulator } from 'firebase/firestore';
 import { getStorage, connectStorageEmulator } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyAZVAziYTWOKYHQg-s3gBzn1YxOLUS0HPk",
+  authDomain: "jivanindiaco.firebaseapp.com",
+  projectId: "jivanindiaco",
+  storageBucket: "jivanindiaco.firebasestorage.app",
+  messagingSenderId: "1028353740894",
+  appId: "1:1028353740894:web:4b184058760c81d8a6a4cf",
+  measurementId: "G-X25J03DP77"
 };
 
 function initializeServices() {
